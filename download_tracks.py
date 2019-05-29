f4ba6aa932155470e529a87102a318269ecb50ea
Jay Harris <jay.harris@outlook.co.nz>
2019-05-29 21:18:50 +1000

    Various clean up

--- download_tracks.py
+++ download_tracks.py
@@ -3,17 +3,19 @@ from lastfm import get_top_tracks
 from youtube_downloader import download_video
 
 def download_tracks(user_name, limit, output_folder):
-    print(output_folder)
     tracks = get_top_tracks(user_name, limit=limit)
 
     for track in tracks:
         youtube_url = youtube_url_for_track(track.item)
 
         if not youtube_url:
-            print("Couldn't parse url for", track.item.name)
+            print("Couldn't parse url for", track.item)
             continue
 
-        download_video(youtube_url)
+        try:
+            download_video(youtube_url, output_folder)
+        except:
+            print("Unabled to download", youtube_url)
 
 if __name__ == '__main__':
     download_tracks('dntbrsnbl')
--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,19 +1,18 @@
 import youtube_dl
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    },
-    {'key': 'FFmpegMetadata'}]
-}
-
-def download_video(youtube_url):
-    print(youtube_url)
+def download_video(youtube_url, output_folder):
+    ydl_opts = {
+        'outtmpl': f'{output_folder}/%(title)s-%(id)s.%(ext)s',
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        },
+        {'key': 'FFmpegMetadata'}]
+    }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([youtube_url])
 
 if __name__ == '__main__':
-    download_video('https://www.youtube.com/watch?v=aCrrsm0U4t0')
+    download_video('https://www.youtube.com/watch?v=aCrrsm0U4t0', 'output')
